fd3579d0f194f11f95db4db6e507f565f163cb03
moshel01 <mos.hel0018@gmail.com>
2026-01-06 09:05:23 -0800

    added gemini + api with hardcoded message test

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
+    if api_key is None:
+        raise RuntimeError("Key not found")
     print("Hello from dumini!")
+    client = genai.Client(api_key=api_key)
+    print(client.models.generate_content(model = 'gemini-2.5-flash', contents = "This is a test of your response. Tell me the weather today in palo alto, california.").text)
 
 
 if __name__ == "__main__":
