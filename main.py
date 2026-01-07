3e0a7ce7c80cf70e57836c7f9f6dd854c9f0f706
moshel01 <mos.hel0018@gmail.com>
2026-01-07 07:32:03 -0800

    added verbose flag

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ def main():
     client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description = "Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role ="user", parts =[types.Part(text=args.user_prompt)])]
-    response_metadata = client.models.generate_content(model = 'gemini-2.5-flash', contents = messages)
-    if response_metadata is None:
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model = 'gemini-2.5-flash', contents = messages)
+    if response is None:
         raise RuntimeError("API failed to return metadata")
-    print(f"Prompt tokens: {response_metadata.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response_metadata.usage_metadata.candidates_token_count}")
-    print(f"Response: {response_metadata.text}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
