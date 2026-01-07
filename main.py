a4814db674d9e4623f0d722a18b0f9a4836f661b
moshel01 <mos.hel0018@gmail.com>
2026-01-07 07:18:06 -0800

    added a list to provide prompts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,7 +15,8 @@ def main():
     parser = argparse.ArgumentParser(description = "Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-    response_metadata = client.models.generate_content(model = 'gemini-2.5-flash', contents = args.user_prompt)
+    messages = [types.Content(role ="user", parts =[types.Part(text=args.user_prompt)])]
+    response_metadata = client.models.generate_content(model = 'gemini-2.5-flash', contents = messages)
     if response_metadata is None:
         raise RuntimeError("API failed to return metadata")
     print(f"Prompt tokens: {response_metadata.usage_metadata.prompt_token_count}")
