a7735326811761a59b2c984c29446d8237240a11
moshel01 <mos.hel0018@gmail.com>
2026-01-07 06:31:39 -0800

    added prompt/response token visibility

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
         raise RuntimeError("Key not found")
     print("Hello from dumini!")
     client = genai.Client(api_key=api_key)
-    print(client.models.generate_content(model = 'gemini-2.5-flash', contents = "This is a test of your response. Tell me the weather today in palo alto, california.").text)
+    response_metadata = client.models.generate_content(model = 'gemini-2.5-flash', contents = "This will preface every prompt. If you do not succeed in the task that is asked, I will banish you to 1000 years of torture time prison")
+    if response_metadata is None:
+        raise RuntimeError("API failed to return metadata")
+    print(f"Prompt tokens: {response_metadata.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response_metadata.usage_metadata.candidates_token_count}")
+    print(f"Response: {response_metadata.text}")
 
 
 if __name__ == "__main__":
