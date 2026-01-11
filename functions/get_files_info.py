6ee34810ea23ded9868ba2e4a5bc8d44cfc6ab9e
moshel01 <mos.hel0018@gmail.com>
2026-01-11 07:33:27 -0800

    added system prompt and ability for LLM to use functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     working_dir_abs = os.path.abspath(working_directory)
@@ -12,3 +13,17 @@ def get_files_info(working_directory, directory="."):
     for thing in os.listdir(target_dir):
         response += f'- {thing}: file_size={os.path.getsize(target_dir + '/' +  thing)} bytes, is_dir={os.path.isdir(target_dir + '/' + thing)}\n'
     return response
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_functions import available_functions
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,14 +20,19 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model = 'gemini-2.5-flash', contents = messages)
+    response = client.models.generate_content(model = 'gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0))
     if response is None:
         raise RuntimeError("API failed to return metadata")
     if args.verbose == True:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+    if response.function_calls != None:
+        for function in response.function_calls:
+            print(f'Calling Function: {function.name}({function.args})')
+    else: 
+        print(f"Response: {response.text}")
+    
 
 
 if __name__ == "__main__":
