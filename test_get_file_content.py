f898841c215ddb9ed4ef3c96da27b84bf628331a
moshel01 <mos.hel0018@gmail.com>
2026-01-11 20:03:46 -0800

    gitignore

--- test_get_file_content.py
+++
@@ -1,6 +0,0 @@
-from functions.get_file_content import *
-print(get_file_content("calculator", "lorem.txt"))
-print(get_file_content("calculator", "main.py"))
-print(get_file_content("calculator", "pkg/calculator.py"))
-print(get_file_content("calculator", "/bin/cat"))
-print(get_file_content("calculator", "pkg/does_not_exist.py"))
